28c8b5326d80535862ed1973d594ff68c7be7821
Elisabeth Shevtsova <lisabeth.shevtsova@gmail.com>
2019-08-19 13:40:08 +0300

    feat / add db module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,29 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { PositionModule } from './position/position.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
     }),
-    DatabaseModule,
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.TYPEORM_DB_HOST || 'localhost',
+        port: parseInt(process.env.TYPEORM_DB_PORT, 10) || 5432,
+        database: process.env.TYPEORM_DB_NAME,
+        username: process.env.TYPEORM_DB_USER,
+        password: process.env.TYPEORM_DB_PASS,
+        entities: [ '**/*.entity.ts' ],
+        synchronize: true,
+    }),
+    PositionModule,
+    EmployeeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
